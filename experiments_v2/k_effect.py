45a3b85f813d9d46d6a6c139618e3786ec0f6285
BigSorry <lexmeulenkamp@hotmail.nl>
2022-08-16 21:51:52 +0200

    change area normalization

--- experiments_v2/k_effect.py
+++ experiments_v2/k_effect.py
@@ -27,6 +27,16 @@ def getDataframe(k_vals, distance_matrix_real, distance_matrix_fake, distance_ma
 
     return datafame
 
+
+def doAreaoperation(real, fake):
+    combined = pd.concat([real, fake])
+
+    real_normed = (real - combined.min()) / \
+                                 (combined.max() - combined.min())
+    fake_normed = (fake - combined.min()) / \
+                                 (combined.max() - combined.min())
+
+    return real_normed, fake_normed
 from sklearn.utils import shuffle
 # TODO Refactoring once results are explained clearly
 def doExperiment():
@@ -35,39 +45,39 @@ def doExperiment():
     samples_both = 1000
     fake_samples = samples_both
     real_samples = samples_both
-    variance = 0.01
+    variance = 0.1
     k_vals = np.linspace(1, 999, 50, dtype=int)
     dimensions = [2, 4, 8, 12, 16, 25, 32, 512]
-    dimensions = [2]
+    dimensions = [2, 4, 16, 32, 64]
     # Prep dataframe
     for dim in dimensions:
         real_data, fake_data = experiment_diverse.getDataNew(real_samples, fake_samples,
                                                              variance, dim)
         distance_matrix_real, distance_matrix_fake, distance_matrix_pairs = helper.getDistanceMatrices(real_data, fake_data)
         dataframe = getDataframe(k_vals, distance_matrix_real, distance_matrix_fake, distance_matrix_pairs, iters)
-        dataframe["real_div_norm"] = (dataframe["area_real"] - dataframe["area_real"].min()) /\
-                                     (dataframe["area_real"].max() - dataframe["area_real"].min())
-        dataframe["fake_div_norm"] = (dataframe["area_fake"] - dataframe["area_fake"].min()) /\
-                                     (dataframe["area_fake"].max() - dataframe["area_fake"].min())
+
+        real_normed, fake_normed = doAreaoperation(dataframe["area_real"], dataframe["area_fake"])
+        dataframe["real_norm"] = real_normed
+        dataframe["fake_norm"] = fake_normed
 
         # Do plotting
+        plt.figure()
+        plt.title(f"(Union data) normalized sum of boundaries with dim is {dim}")
+        plt.plot(dataframe["k_val"], dataframe["real_norm"], label="real")
+        plt.plot(dataframe["k_val"], dataframe["fake_norm"], label="fake")
+        plt.ylim([0, 1.1])
+        plt.xscale('log')
+        plt.legend()
+        plt.figure()
+        plt.title(f"Scores with dim is {dim}")
+        plt.plot(dataframe["k_val"], dataframe["recall"], label="recall")
+        plt.plot(dataframe["k_val"], dataframe["coverage"], label=" coverage")
+        plt.ylim([0, 1.1])
+        plt.xscale('log')
+        plt.legend()
         if dim == 2:
-            plt.figure()
-            plt.title(f"Sum of all Areas with dim is {dim}")
-            plt.plot(dataframe["k_val"], dataframe["real_div_norm"], label="real")
-            plt.plot(dataframe["k_val"], dataframe["fake_div_norm"], label="fake")
-            plt.ylim([0, 1.1])
-            plt.xscale('log')
-            plt.legend()
-            plt.figure()
-            plt.title(f"Scores with dim is {dim}")
-            plt.plot(dataframe["k_val"], dataframe["recall"], label="recall")
-            plt.plot(dataframe["k_val"], dataframe["coverage"], label=" coverage")
-            plt.ylim([0, 1.1])
-            plt.xscale('log')
-            plt.legend()
             for index, k in enumerate(k_vals):
-                if index % 10 == 0:
+                if index % 20 == 0:
                     data_dict = {"real_data":[real_data], "fake_data":[fake_data], "k_val":[k], "titles":[f"K-val is {k}"]}
                     plotting.plotInterface(data_dict, save=False, save_path="")
 
