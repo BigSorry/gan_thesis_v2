ef00648d98328926fc258bcc1d7753864c3e2626
BigSorry <lexmeulenkamp@hotmail.nl>
2022-09-08 13:54:10 +0200

    add multiple dimensions to exper

--- experiments_v2/sample_experiment.py
+++ experiments_v2/sample_experiment.py
@@ -13,73 +13,77 @@ def getKParams(sample_size, max_k, step_size=10):
         vals.append(sample_size-1)
     return vals
 
-def getData(iters, dimension, sample_sizes, lambda_factors):
+def getData(iters, dimensions, sample_sizes, lambda_factors):
     columns = ["iter", "sample_size", "dimension", "lambda",
                "k_val", "recall", "coverage"]
     row_data = []
-    mean = np.zeros(dimension)
     for iter in range(iters):
         for samples in sample_sizes:
             k_vals = getKParams(samples, max_k=int(samples), step_size=20)
             print(k_vals)
-            for scale_factor in lambda_factors:
-                cov_real = np.eye(dimension)
-                cov_fake = np.eye(dimension) * scale_factor
-                real_features = np.random.multivariate_normal(mean, cov_real, size=samples)
-                fake_features = np.random.multivariate_normal(mean, cov_fake, size=samples)
-                distance_matrix_real, distance_matrix_fake, distance_matrix_pairs = util.getDistanceMatrices(
-                    real_features, fake_features)
-                for k_val in k_vals:
-                    # Calculations
-                    boundaries_real = distance_matrix_real[:, k_val]
-                    boundaries_fake = distance_matrix_real[:, k_val]
-                    precision, recall, density, coverage = util.getScores(distance_matrix_pairs, boundaries_fake,
-                                                                          boundaries_real, k_val)
-                    row = [iter, samples, dimension, scale_factor, k_val, recall, coverage]
-                    row_data.append(row)
+            for dimension in dimensions:
+                mean_vec = np.zeros(dimension)
+                for scale_factor in lambda_factors:
+                    cov_real = np.eye(dimension)
+                    cov_fake = np.eye(dimension) * scale_factor
+                    real_features = np.random.multivariate_normal(mean_vec, cov_real, size=samples)
+                    fake_features = np.random.multivariate_normal(mean_vec, cov_fake, size=samples)
+                    distance_matrix_real, distance_matrix_fake, distance_matrix_pairs = util.getDistanceMatrices(
+                        real_features, fake_features)
+                    for k_val in k_vals:
+                        # Calculations
+                        boundaries_real = distance_matrix_real[:, k_val]
+                        boundaries_fake = distance_matrix_real[:, k_val]
+                        precision, recall, density, coverage = util.getScores(distance_matrix_pairs, boundaries_fake,
+                                                                              boundaries_real, k_val)
+                        row = [iter, samples, dimension, scale_factor, k_val, recall, coverage]
+                        row_data.append(row)
 
     dataframe = pd.DataFrame(columns=columns, data=row_data)
     return dataframe
 
 def plotExperiment(dataframe, sample_size, show_boxplot,
                    show_map, path_box, path_map):
-        if show_boxplot:
-            grouped_data = dataframe.groupby(["k_val"])
-            recalls = []
-            coverages = []
-            xticks = []
-            for k_val, group_data in grouped_data:
-                recall = group_data["recall"].values
-                coverage = group_data["coverage"].values
-                recalls.append(recall)
-                coverages.append(coverage)
-                xticks.append(k_val)
+    effective_sample_size = sample_size / dataframe["dimension"].iloc[0]
+    recall_text = f"Recall with sample size {sample_size} with ess {effective_sample_size}"
+    coverage_text = f"Recall with sample size {sample_size} with ess {effective_sample_size}"
+    if show_boxplot:
+        grouped_data = dataframe.groupby(["k_val"])
+        recalls = []
+        coverages = []
+        xticks = []
+        for k_val, group_data in grouped_data:
+            recall = group_data["recall"].values
+            coverage = group_data["coverage"].values
+            recalls.append(recall)
+            coverages.append(coverage)
+            xticks.append(k_val)
 
-            plotting.plotBox(recalls, xticks, f"Recall with sample size {sample_size}",
-                             save=True, save_path=path_box+f"recall_samples{sample_size}.png")
-            plotting.plotBox(coverages, xticks, f"Coverages with sample size {sample_size}",
-                             save=True, save_path=path_box+f"coverage_samples{sample_size}.png")
+        plotting.plotBox(recalls, xticks, recall_text,
+                         save=True, save_path=path_box+f"recall_samples{sample_size}.png")
+        plotting.plotBox(coverages, xticks, coverage_text,
+                         save=True, save_path=path_box+f"coverage_samples{sample_size}.png")
 
-        if show_map:
-            recall_pivot = pd.pivot_table(dataframe, values='recall', index=['lambda'],
-                    columns=['k_val'], aggfunc=np.mean)
-            coverage_pivot = pd.pivot_table(dataframe, values='coverage', index=['lambda'],
-                                          columns=['k_val'], aggfunc=np.mean)
+    if show_map:
+        recall_pivot = pd.pivot_table(dataframe, values='recall', index=['lambda'],
+                columns=['k_val'], aggfunc=np.mean)
+        coverage_pivot = pd.pivot_table(dataframe, values='coverage', index=['lambda'],
+                                      columns=['k_val'], aggfunc=np.mean)
 
-            plotting.HeatMapPivot(recall_pivot, title_text=f"Recall with sample size {sample_size}",
-                                  save=True, save_path=path_map+f"recall_samples{sample_size}.png")
-            plotting.HeatMapPivot(coverage_pivot, title_text=f"Coverage with sample size {sample_size}",
-                                  save=True, save_path=path_map+f"coverage_samples{sample_size}.png")
+        plotting.HeatMapPivot(recall_pivot, title_text=recall_text,
+                              save=True, save_path=path_map+f"recall_samples{sample_size}.png")
+        plotting.HeatMapPivot(coverage_pivot, title_text=coverage_text,
+                              save=True, save_path=path_map+f"coverage_samples{sample_size}.png")
 
 
 import seaborn as sns
 def sampleExperiment():
     # Data Setup
     iters = 2
-    dimension = 64
+    dimensions = [2, 16, 32]
     sample_sizes = [500, 1000]
     lambda_factors = [0.01, 0.1, 10, 100]
-    dataframe = getData(iters, dimension, sample_sizes, lambda_factors)
+    dataframe = getData(iters, dimensions, sample_sizes, lambda_factors)
 
     show_box=True
     show_map=True
@@ -89,8 +93,10 @@ def sampleExperiment():
     path_map = "C:/Users/lexme/OneDrive/plots_thesis/laptop/heatmap/"
     path_box = "C:/Users/lexme/OneDrive/plots_thesis/laptop/boxplot/"
     for sample_size in sample_sizes:
-            select_data = dataframe.loc[dataframe["sample_size"] == sample_size, :]
-            plotExperiment(select_data, sample_size, show_box,
-                           show_map, path_box, path_map)
+        for dimension in dimensions:
+                select_data = dataframe.loc[(dataframe["sample_size"] == sample_size) &
+                                            (dataframe["dimension"] == dimension) , :]
+                plotExperiment(select_data, sample_size, show_box,
+                               show_map, path_box, path_map)
 
 
