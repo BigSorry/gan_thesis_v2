b9a3dc8e7f145eacdb244c00d605c55cb8951245
BigSorry <lexmeulenkamp@hotmail.nl>
2023-02-23 13:26:54 +0100

    more refactoring and different grouping method for experiment data

--- experiments_v2/curve/run_configs.py
+++ experiments_v2/curve/run_configs.py
@@ -4,34 +4,32 @@ import union_experiment as exp
 import matplotlib.pyplot as plt
 import visualize as plotting
 
-def plotHeatMaps(first_pivot, second_pivot, title_text, save_path):
+# TODO Refactoring
+def plotHeatMaps(dataframe, map_path, sample_size):
+    pr_first_pivot = dataframe.pivot(index="lambda_factor", columns="dimension", values="pr_nearest_distances")
+    pr_second_pivot = dataframe.pivot(index="lambda_factor", columns="dimension", values="pr_under_mean")
+    dc_first_pivot = dataframe.pivot(index="lambda_factor", columns="dimension", values="dc_nearest_distances")
+    dc_second_pivot = dataframe.pivot(index="lambda_factor", columns="dimension", values="dc_under_mean")
+    # Precision and Recall
+    pr_save_path = f"{map_path}pr_s{sample_size}_.png"
     plt.figure(figsize=(14, 6))
     plt.subplot(2, 1, 1)
-    plotting.HeatMapPivot(first_pivot, title_text=f"{title_text} "
+    plotting.HeatMapPivot(pr_first_pivot, title_text=f"Precision and Recall with samples {sample_size} \n"
                                                   f"mean l1 distance between pr and nearest theoretical point",
-                          save=False, save_path=save_path)
+                          save=False, save_path=pr_save_path)
     plt.subplot(2, 1, 2)
-    plotting.HeatMapPivot(second_pivot, title_text=f"{title_text}"
-                                                   f"percentage points overestimation",
-                          save=True, save_path=save_path)
-def plotHeatmaps(dataframe, sample_size):
-    # Precision and Recall heatmaps
-    map_path = "C:/Users/lexme/Documents/gan_thesis_v2/images/"
-
-    pr_pivot = dataframe.pivot(index="lambda_factor", columns="dimension",
-                                      values="pr_nearest_distances")
-    pr_pivot2 = dataframe.pivot(index="lambda_factor", columns="dimension", values="pr_under_mean")
-    title_text = f"Precision and Recall, samples {sample_size}, \n"
-    save_path = f"{map_path}pr_s{sample_size}.png"
-    plotHeatMaps(pr_pivot, pr_pivot2, title_text, save_path)
-
-    # Density and Coverage heatmaps
-    dc_pivot = dataframe.pivot(index="lambda_factor", columns="dimension",
-                                      values="dc_nearest_distances")
-    dc_pivot2 = dataframe.pivot(index="lambda_factor", columns="dimension", values="dc_under_mean")
-    title_text = f"Density and Coverage, samples {sample_size}, \n"
-    save_path = f"{map_path}dc_s{sample_size}.png"
-    plotHeatMaps(dc_pivot,dc_pivot2, title_text, save_path)
+    plotting.HeatMapPivot(pr_second_pivot, title_text=f"percentage points overestimation",
+                          save=True, save_path=pr_save_path)
+    # Density and Coverage
+    dc_save_path = f"{map_path}dc_s{sample_size}.png"
+    plt.figure(figsize=(14, 6))
+    plt.subplot(2, 1, 1)
+    plotting.HeatMapPivot(dc_first_pivot, title_text=f"Density and Coverage with samples {sample_size} \n"
+                                                     f"mean l1 distance between pr and nearest theoretical point",
+                          save=False, save_path=dc_save_path)
+    plt.subplot(2, 1, 2)
+    plotting.HeatMapPivot(dc_second_pivot, title_text=f"percentage points overestimation",
+                          save=True, save_path=dc_save_path)
 
 def checkScaling():
     samples=1000
@@ -44,13 +42,14 @@ def checkScaling():
         var_factors *= 10
 
 def runExperiment():
+    map_path = "C:/Users/lexme/Documents/gan_thesis_v2/images/"
     headers = ["dimension", "lambda_factor", "pr_under_mean", "dc_under_mean",
-               "pr_nearest_distances", "dc_nearest_distances"]
+               "pr_nearest_distances", "dc_nearest_distances", "real_scaling"]
     row_values = []
     sample_sizes = [1000, 3000, 5000]
-    sample_sizes = [1000]
-    dimensions = [2, 8, 16, 32]
-    lambda_factors = np.array([0.01, 0.1, 0.2, 0.25, 0.5, 0.75, 1])
+    #sample_sizes = [1000]
+    dimensions = [2, 8, 16, 32, 64]
+    lambda_factors = np.array([0.01, 0.25, 0.5, 0.75, 1])
     for sample_size in sample_sizes:
         k_vals = np.array([1, 2, 3, 4, 8, 9, 16, 32, 64, sample_size - 1])
         for dimension in dimensions:
@@ -60,10 +59,19 @@ def runExperiment():
                 scale_factor = lambda_factors[index]
                 stat_values = exp.doExperiment(reference_distribution, distribution, constant_factor, scale_factor,
                                  k_vals, save_curve=False)
-                row = [dimension, scale_factor] + stat_values
+                stat_values_reverse = exp.doExperiment(distribution, reference_distribution, scale_factor, constant_factor,
+                                               k_vals, save_curve=False)
+
+                row = [dimension, scale_factor] + stat_values + [True]
+                row_reverse = [dimension, scale_factor] + stat_values_reverse + [False]
                 row_values.append(row)
+                row_values.append(row_reverse)
 
         dataframe = pd.DataFrame(data=row_values, columns=headers)
-        plotHeatmaps(dataframe, sample_size)
+        real_scaled = dataframe.loc[dataframe["real_scaling"] == True, :]
+        fake_scaled_scaled = dataframe.loc[dataframe["real_scaling"] == True, :]
+        plotHeatMaps(real_scaled, map_path, sample_size)
+        plotHeatMaps(fake_scaled_scaled, map_path, sample_size)
+
 
 runExperiment()
