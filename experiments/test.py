a3b11c913dfef8481e260c7841ddc8938c48effd
BigSorry <lexmeulenkamp@hotmail.nl>
2023-03-08 09:19:45 +0100

    add test script

--- experiments/test.py
+++ experiments/test.py
@@ -3,25 +3,34 @@ import matplotlib.pyplot as plt
 from experiments import create_experiment as exp
 
 def getData():
-    sample_sizes = [2, 4, 8, 16, 32, 64, 512, 1024]
-    dimensions = [2, 4, 8, 16, 32, 64, 512, 1024]
-    k_vals = np.array([1, 3, 7, 9, 16, 32, 64, num_real_samples - 1])
-    for dimension in dimensions:
-        real_features = np.random.normal(loc=0.0, scale=1.0,
-                                         size=[num_real_samples, dimension])
+    sample_sizes = [1000]
+    dimensions = [2]
+    outlier_value = 0 + np.sqrt(1)
+    for samples in sample_sizes:
+        k_vals = np.array([1, 3, 7, 9, 16, 32, 64, 128])
+        for dimension in dimensions:
+            real_features = np.random.normal(loc=0.0, scale=1.0,
+                                             size=[samples, dimension])
 
-        fake_features = np.random.normal(loc=0.0, scale=1.0,
-                                         size=[num_fake_samples, dimension])
+            fake_features = np.random.normal(loc=0.0, scale=1.0,
+                                             size=[samples, dimension])
 
-        map_path = f"./gaussian_curve/"
-        pr_aboves, dc_aboves, pr_nearest_distances, dc_nearest_distances = exp.doExperiment("gaussian",
-                                                                                            real_features,
-                                                                                            fake_features,
-                                                                                            1, 1,
-                                                                                            k_vals,
-                                                                                            save_curve=True,
-                                                                                            map_path=map_path)
+            outlier_boolean = (np.abs(real_features) > outlier_value).any(axis=1)
+            real_inliers = real_features[~outlier_boolean, :]
+            real_outlier = real_features[outlier_boolean, :]
 
-    plt.show()
+            plt.figure()
+            plt.scatter(real_outlier[:, 0], real_outlier[:, 1], c="red")
+            plt.scatter(real_inliers[:, 0], real_inliers[:, 1], c="blue")
+            plt.show()
+            map_path = f"../gaussian_outlier/"
+            pr_aboves, dc_aboves, pr_nearest_distances, dc_nearest_distances = exp.doExperiment("gaussian",
+                                                                                                real_features,
+                                                                                                real_inliers,
+                                                                                                1, 1,
+                                                                                                k_vals,
+                                                                                                save_curve=True,
+                                                                                                map_path=map_path)
+
 
 getData()
