6b85c89abaefc6f5a2b624fda94de7fc181c6a20
BigSorry <lexmeulenkamp@hotmail.nl>
2023-03-09 15:59:16 +0100

    add tail exp to test.py

--- experiments/test.py
+++ experiments/test.py
@@ -1,36 +1,61 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from experiments import create_experiment as exp
+from scipy.spatial import distance
+
+def resample(needed_samples, outlier_distance, dimension):
+    new_samples = []
+    while len(new_samples) < needed_samples:
+        mean_vec = np.zeros(dimension)
+        identity_cov = np.eye(dimension)
+        real_features = np.random.multivariate_normal(mean_vec, identity_cov, 5000)
+        for vector in real_features:
+            mal_distance = distance.mahalanobis(vector, mean_vec, identity_cov)
+            if mal_distance > outlier_distance:
+                new_samples.append(vector)
+    new_samples = np.array(new_samples)
+    return new_samples[:needed_samples, :]
 
 def getData():
-    sample_sizes = [1000]
-    dimensions = [2]
-    outlier_value = 0 + np.sqrt(1)
+    sample_sizes = [5000]
+    dimensions = [2, 8, 16, 32, 64, 512, 1024]
     for samples in sample_sizes:
-        k_vals = np.array([1, 3, 7, 9, 16, 32, 64, 128])
-        for dimension in dimensions:
-            real_features = np.random.normal(loc=0.0, scale=1.0,
-                                             size=[samples, dimension])
+        k_vals = np.array([1, 3, 7, 9, 16, 32])
+        for index, dimension in enumerate(dimensions):
+            mean_vec = np.zeros(dimension)
+            identity_cov = np.eye(dimension)
+            real_features = np.random.multivariate_normal(mean_vec, identity_cov, samples)
+            distances = []
+            for vector in real_features:
+                mal_distance = distance.mahalanobis(vector, mean_vec, identity_cov)
+                distances.append(mal_distance)
+            distances = np.array(distances)
+            outlier_value = np.quantile(distances, 0.95)
+            print(outlier_value)
+            boolean_mask = distances > outlier_value
+            inliers = real_features[~boolean_mask, :]
+            outliers = resample(inliers.shape[0], outlier_value, dimension)
 
-            fake_features = np.random.normal(loc=0.0, scale=1.0,
-                                             size=[samples, dimension])
 
-            outlier_boolean = (np.abs(real_features) > outlier_value).any(axis=1)
-            real_inliers = real_features[~outlier_boolean, :]
-            real_outlier = real_features[outlier_boolean, :]
-
-            plt.figure()
-            plt.scatter(real_outlier[:, 0], real_outlier[:, 1], c="red")
-            plt.scatter(real_inliers[:, 0], real_inliers[:, 1], c="blue")
-            plt.show()
-            map_path = f"../gaussian_outlier/"
+            map_path = f"../gaussian_outlier/standard"
             pr_aboves, dc_aboves, pr_nearest_distances, dc_nearest_distances = exp.doExperiment("gaussian",
-                                                                                                real_features,
-                                                                                                real_inliers,
+                                                                                                inliers,
+                                                                                                outliers,
+                                                                                                1, 1,
+                                                                                                k_vals,
+                                                                                                save_curve=True,
+                                                                                                map_path=map_path)
+            map_path = f"../gaussian_outlier/reverse/"
+            pr_aboves, dc_aboves, pr_nearest_distances, dc_nearest_distances = exp.doExperiment("gaussian",
+                                                                                                outliers,
+                                                                                                inliers,
                                                                                                 1, 1,
                                                                                                 k_vals,
                                                                                                 save_curve=True,
                                                                                                 map_path=map_path)
 
 
+
+
 getData()
+plt.show()
