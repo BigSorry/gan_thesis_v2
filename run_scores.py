daeaf293bc6ba80908963d0758820301d8acb011
BigSorry <lexmeulenkamp@hotmail.nl>
2023-03-29 19:01:29 +0200

    update script

--- run_scores.py
+++ run_scores.py
@@ -9,6 +9,7 @@ import experiments.experiment_visualization as exp_vis
 def plotLine(dataframe, dimensions):
     grouped = dataframe.groupby(["dimension", "lambda_factor"]).mean().reset_index()
     score_names = ["precision", "recall", "density", "coverage"]
+    score_names = ["recall", "coverage"]
     for dim in dimensions:
         sel_data = grouped.loc[grouped["dimension"] == dim, :]
         plt.figure()
@@ -23,6 +24,17 @@ def plotLine(dataframe, dimensions):
 
     plt.show()
 
+
+def getDistributions(iterations, sample_sizes, dimensions, lambda_factors, distribution_name):
+    dict = {}
+    for iter in range(iterations):
+        for samples in sample_sizes:
+            for dim in dimensions:
+                key = (iter, samples, dim)
+                reference_distribution, scaled_distributions = dist.getDensities(samples, dim, lambda_factors,
+                                                                     distribution_name=distribution_name)
+                dict[key] = {"reference":reference_distribution, "scaled": scaled_distributions}
+    return dict
 def gaussianLine():
     # Key is name which is scaled and value corresponding value for method calling
     scaling_info = {"fake_scaled": False, "real_scaled": True}
@@ -40,27 +52,26 @@ def gaussianLine():
     headers = ["iteration", "dimension", "lambda_factor", "k_val", "precision",
                "recall", "density", "coverage"]
     all_rows = []
+    distribution_dict = getDistributions(iterations, sample_sizes, dimensions, lambda_factors, distribution_name)
+    for key, value in distribution_dict.items():
+        iter = key[0]
+        dim = key[2]
+        reference_distribution = value["reference"]
+        scaled_distributions = value["scaled"]
 
-    for iter in range(iterations):
-        for samples in sample_sizes:
-            for dim in dimensions:
-                reference_distribution, scaled_distributions = dist.getDensities(samples, dim, lambda_factors,
-                                                                     distribution_name=distribution_name)
-                for index, scaled_distribution in enumerate(scaled_distributions):
-                    pr_pairs, dc_pairs = exp.getKNN(reference_distribution, scaled_distribution, k_vals)
+        for index, scaled_distribution in enumerate(scaled_distributions):
+            pr_pairs, dc_pairs = exp.getKNN(reference_distribution, scaled_distribution, k_vals)
 
-                    scale_factors = [lambda_factors[0], lambda_factors[index]]
-                    # indexcurve_classifier, curve_var_dist = exp.getGroundTruth(distribution_name, reference_distribution,
-                    #                                                       scaled_distribution, scale_factors)
-                    for i in range(len(k_vals)):
-                        precision = pr_pairs[i, 0]
-                        recall = pr_pairs[i, 1]
-                        density = dc_pairs[i, 0]
-                        coverage = dc_pairs[i, 1]
+            scale_factors = [lambda_factors[0], lambda_factors[index]]
+            for i in range(len(k_vals)):
+                precision = pr_pairs[i, 0]
+                recall = pr_pairs[i, 1]
+                density = dc_pairs[i, 0]
+                coverage = dc_pairs[i, 1]
 
 
-                        all_rows.append([iter, dim, scale_factors[1], k_vals[i],
-                                         precision, recall, density, coverage])
+                all_rows.append([iter, dim, scale_factors[1], k_vals[i],
+                                 precision, recall, density, coverage])
 
     dataframe = pd.DataFrame(data=all_rows, columns=headers)
     plotLine(dataframe, dimensions)
